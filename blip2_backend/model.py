fabb8841830294f08914ab034e397c18c3841b5f
yekta <ngyekta@gmail.com>
2023-08-30 08:38:46 +0300

    Blip2 backend

---
+++ blip2_backend/model.py
@@ -0,0 +1,80 @@
+from typing import List, Dict, Optional
+from label_studio_ml.model import LabelStudioMLBase
+from transformers import AutoProcessor, Blip2ForConditionalGeneration
+from PIL import Image
+import requests
+
+
+device = "cpu"
+
+
+class NewModel(LabelStudioMLBase):
+    def __init__(self, project_id, **kwargs):
+        super(NewModel, self).__init__(**kwargs)
+        self.processor = AutoProcessor.from_pretrained("Salesforce/blip2-opt-2.7b")
+        self.model = Blip2ForConditionalGeneration.from_pretrained(
+            "Salesforce/blip2-opt-2.7b"
+        ).to(device)
+
+    def predict(
+        self, tasks: List[Dict], context: Optional[Dict] = None, **kwargs
+    ) -> List[Dict]:
+        predictions = []
+        from_name, schema = list(self.parsed_label_config.items())[0]
+        to_name = schema["to_name"][0]
+        for task in tasks:
+            image_url = task["data"]["image"]
+            image = Image.open(requests.get(image_url, stream=True).raw)
+            inputs = self.processor(image).to(device)
+            generated_ids = self.model.generate(**inputs, max_new_tokens=20)
+            generated_text = self.processor.batch_decode(
+                generated_ids, skip_special_tokens=True
+            )[0].strip()
+            prediction = [
+                {
+                    "type": "textarea",
+                    "value": {"text": [generated_text]},
+                    "to_name": to_name,
+                    "from_name": from_name,
+                }
+            ]
+            predictions.append(prediction)
+
+        """Write your inference logic here
+        :param tasks: [Label Studio tasks in JSON format](https://labelstud.io/guide/task_format.html)
+        :param context: [Label Studio context in JSON format](https://labelstud.io/guide/ml.html#Passing-data-to-ML-backend)
+        :return predictions: [Predictions array in JSON format](https://labelstud.io/guide/export.html#Raw-JSON-format-of-completed-tasks)
+        """
+        print(
+            f"""\
+        Run prediction on {tasks}
+        Received context: {context}
+        Project ID: {self.project_id}
+        Label config: {self.label_config}
+        Parsed JSON Label config: {self.parsed_label_config}"""
+        )
+        return predictions
+
+    def fit(self, event, data, **kwargs):
+        """
+        This method is called each time an annotation is created or updated
+        You can run your logic here to update the model and persist it to the cache
+        It is not recommended to perform long-running operations here, as it will block the main thread
+        Instead, consider running a separate process or a thread (like RQ worker) to perform the training
+        :param event: event type can be ('ANNOTATION_CREATED', 'ANNOTATION_UPDATED')
+        :param data: the payload received from the event (check [Webhook event reference](https://labelstud.io/guide/webhook_reference.html))
+        """
+
+        # use cache to retrieve the data from the previous fit() runs
+        old_data = self.get("my_data")
+        old_model_version = self.get("model_version")
+        print(f"Old data: {old_data}")
+        print(f"Old model version: {old_model_version}")
+
+        # store new data to the cache
+        self.set("my_data", "my_new_data_value")
+        self.set("model_version", "my_new_model_version")
+        print(f'New data: {self.get("my_data")}')
+        print(f'New model version: {self.get("model_version")}')
+
+        print("fit() completed successfully.")
--- label_studio_ml/api.py
+++ label_studio_ml/api.py
@@ -16,13 +16,16 @@ def init_app(model_class):
     global MODEL_CLASS
 
     if not issubclass(model_class, LabelStudioMLBase):
-        raise ValueError('Inference class should be the subclass of ' + LabelStudioMLBase.__class__.__name__)
+        raise ValueError(
+            "Inference class should be the subclass of "
+            + LabelStudioMLBase.__class__.__name__
+        )
 
     MODEL_CLASS = model_class
     return _server
 
 
-@_server.route('/predict', methods=['POST'])
+@_server.route("/predict", methods=["POST"])
 @exception_handler
 def _predict():
     """
@@ -45,68 +48,65 @@ def _predict():
     Predictions in LS format
     """
     data = request.json
-    tasks = data.get('tasks')
-    params = data.get('params') or {}
-    project = data.get('project')
+    tasks = data.get("tasks")
+    params = data.get("params") or {}
+    project = data.get("project")
     if project:
-        project_id = data.get('project').split('.', 1)[0]
+        project_id = data.get("project").split(".", 1)[0]
     else:
         project_id = None
-    label_config = data.get('label_config')
-    context = params.pop('context', {})
+    label_config = data.get("label_config")
+    context = params.pop("context", {})
 
     model = MODEL_CLASS(project_id)
     model.use_label_config(label_config)
 
     predictions = model.predict(tasks, context=context, **params)
-    return jsonify({'results': predictions})
+    return jsonify({"results": predictions})
 
 
-@_server.route('/setup', methods=['POST'])
+@_server.route("/setup", methods=["POST"])
 @exception_handler
 def _setup():
     data = request.json
-    project_id = data.get('project').split('.', 1)[0]
-    label_config = data.get('schema')
+    project_id = data.get("project").split(".", 1)[0]
+    label_config = data.get("schema")
     model = MODEL_CLASS(project_id)
     model.use_label_config(label_config)
-    model_version = model.get('model_version')
-    return jsonify({'model_version': model_version})
+    model_version = model.get("model_version")
+    return jsonify({"model_version": model_version})
 
 
 TRAIN_EVENTS = (
-    'ANNOTATION_CREATED',
-    'ANNOTATION_UPDATED',
-    'ANNOTATION_DELETED',
-    'PROJECT_UPDATED'
+    "ANNOTATION_CREATED",
+    "ANNOTATION_UPDATED",
+    "ANNOTATION_DELETED",
+    "PROJECT_UPDATED",
 )
 
 
-@_server.route('/webhook', methods=['POST'])
+@_server.route("/webhook", methods=["POST"])
 def webhook():
     data = request.json
-    event = data.pop('action')
+    event = data.pop("action")
     if event not in TRAIN_EVENTS:
-        return jsonify({'status': 'Unknown event'}), 200
-    project_id = str(data['project']['id'])
-    label_config = data['project']['label_config']
+        return jsonify({"status": "Unknown event"}), 200
+    project_id = str(data["project"]["id"])
+    label_config = data["project"]["label_config"]
     model = MODEL_CLASS(project_id)
     model.use_label_config(label_config)
     model.fit(event, data)
     return jsonify({}), 201
 
 
-@_server.route('/health', methods=['GET'])
-@_server.route('/', methods=['GET'])
+@_server.route("/health", methods=["GET"])
+@_server.route("/", methods=["GET"])
 @exception_handler
 def health():
-    return jsonify({
-        'status': 'UP',
-        'model_class': MODEL_CLASS.__name__
-    })
+    return jsonify({"status": "UP", "model_class": MODEL_CLASS.__name__})
 
 
-@_server.route('/metrics', methods=['GET'])
+@_server.route("/metrics", methods=["GET"])
 @exception_handler
 def metrics():
     return jsonify({})
@@ -114,7 +114,7 @@ def metrics():
 
 @_server.errorhandler(FileNotFoundError)
 def file_not_found_error_handler(error):
-    logger.warning('Got error: ' + str(error))
+    logger.warning("Got error: " + str(error))
     return str(error), 404
 
 
@@ -132,13 +132,13 @@ def index_error(error):
 
 @_server.before_request
 def log_request_info():
-    logger.debug('Request headers: %s', request.headers)
-    logger.debug('Request body: %s', request.get_data())
+    logger.debug("Request headers: %s", request.headers)
+    logger.debug("Request body: %s", request.get_data())
 
 
 @_server.after_request
 def log_response_info(response):
-    logger.debug('Response status: %s', response.status)
-    logger.debug('Response headers: %s', response.headers)
-    logger.debug('Response body: %s', response.get_data())
+    logger.debug("Response status: %s", response.status)
+    logger.debug("Response headers: %s", response.headers)
+    logger.debug("Response body: %s", response.get_data())
     return response
