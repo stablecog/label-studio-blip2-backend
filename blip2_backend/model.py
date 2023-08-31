2cf91f7885f30fc47c35ccee4fa02f0bbe188cb0
yekta <ngyekta@gmail.com>
2023-08-31 05:13:30 +0300

    Change max length

--- blip2_backend/model.py
+++ blip2_backend/model.py
@@ -20,12 +20,12 @@ device = "cpu"
 processor_pre = None
 model_pre = None
 model_settings = {
-    "max_length": 30,
+    "max_length": 40,
     "min_length": 10,
     "top_p": 0.9,
     "num_beams": 4,
     "repetition_penalty": 1.5,
-    "length_penalty": 2.0,
+    "length_penalty": 1.5,
     "do_sample": False,
 }
 
